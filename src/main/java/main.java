ffa12a02737c886539f5db560898a322875fac35
Andreas <git@an-9.de>
2021-11-15 22:48:56 +0100

    Removed main which was used for testing

--- src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        PythonParser parser = new PythonParser();
-        parser.addVariable("a", "1");
-        parser.evaluate("1");
-    }
-}
